588d9d08328662ec3b1366e9ff8124ac18bc1ae5
Whalter Duarte <dev@whalter.com.br>
2024-01-23 14:09:23 -0300

    Configurando api

--- components/userDetails/UserDetails.tsx
+++ components/userDetails/UserDetails.tsx
@@ -29,7 +29,7 @@ const UserDetails: React.FC<UserComponentProps> = ({ session }) => {
     const fetchToken = async () => {
       try {
         if (session) {
-          const res = await axios.get("http://localhost:3000/api/auth/session");
+          const res = await axios.get(`${process.env.BASE}/api/auth/session`);
           const sessionData = res.data;
           setToken(sessionData.user.token);
         }
