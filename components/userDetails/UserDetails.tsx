3c32fb8210003b88e6e44bee57d28d5bb8859e12
Whalter Duarte <dev@whalter.com.br>
2024-01-23 14:17:01 -0300

    Configurando api

--- components/userDetails/UserDetails.tsx
+++ components/userDetails/UserDetails.tsx
@@ -29,7 +29,9 @@ const UserDetails: React.FC<UserComponentProps> = ({ session }) => {
     const fetchToken = async () => {
       try {
         if (session) {
-          const res = await axios.get(`${process.env.BASE}/api/auth/session`);
+          const res = await axios.get(
+            `https://api-byte.vercel.app/api/auth/session`
+          );
           const sessionData = res.data;
           setToken(sessionData.user.token);
         }
